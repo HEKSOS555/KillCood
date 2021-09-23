7b1058defabc349180f322d969a1f5aa9f5fa605
HEKSOS <86925042+HEKSOS555@users.noreply.github.com>
2021-09-24 01:35:41 +0200

    Add files via upload

---
+++ KilliCood.py
@@ -0,0 +1,63 @@
+import os
+os.system ("clear")
+import random
+import getpass
+import sys
+
+#colors 
+GG = "\033[32;1m" # Green light
+RR = "\033[31;1m" # Red light
+
+print("")
+print(RR+"          Welcome The Tool killi Cood")
+print(GG+"""
+                uu$$$$$$$$$$$uu
+             uu$$$$$$$$$$$$$$$$$uu
+            u$$$$$$$$$$$$$$$$$$$$$u
+           u$$$$$$$$$$$$$$$$$$$$$$$u
+          u$$$$$$$$$$$$$$$$$$$$$$$$$u
+          u$$$$$$$$$$$$$$$$$$$$$$$$$u
+          u$$$$$$“   “$$$“   “$$$$$$u
+          “$$$$“      u$u       $$$$“
+           $$$u       u$u       u$$$
+           $$$u      u$$$u      u$$$
+            “$$$$uu$$$   $$$uu$$$$“
+             “$$$$$$$“   “$$$$$$$“
+               u$$$$$$$u$$$$$$$u
+                u$“$“$“$“$“$“$u
+     uuu        $$u$ $ $ $ $u$$       uuu
+    u$$$$        $$$$$u$u$u$$$       u$$$$
+     $$$$$uu      “$$$$$$$$$“     uu$$$$$$
+   u$$$$$$$$$$$uu    “““““    uuuu$$$$$$$$$$
+   $$$$“““$$$$$$$$$$uuu   uu$$$$$$$$$“““$$$“
+    “““      ““$$$$$$$$$$$uu ““$“““
+              uuuu ““$$$$$$$$$$uuu
+     u$$$uuu$$$$$$$$$uu ““$$$$$$$$$$$uuu$$$
+     $$$$$$$$$$““““           ““$$$$$$$$$$$“
+      “$$$$$“                      ““$$$$““
+        $$$“                         $$$$“""")
+
+print("")
+print("")
+nmpr = input(RR+"Enter the Number Target : ")
+print(GG+"")
+def GetPassword(data):
+	Max = 6
+	password=""
+	while len(password) != Max:
+		value = random.choice(data)
+		password += value
+	return password
+
+data ='1234567890'
+for i in range(15):
+	print(GetPassword(data))
+
+
+print(RR+"")
+print("======================")
+print("     by_HEKSOS")
+print("======================")
+print(" YouTube : MR.HEKSOS")
+print("======================")
+print("")
