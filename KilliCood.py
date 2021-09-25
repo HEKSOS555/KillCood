bb6d3c99256aa56bafe54c9f8ccc302827c68329
HEKSOS <86925042+HEKSOS555@users.noreply.github.com>
2021-09-25 16:59:33 +0200

    Add files via upload

--- KilliCood.py
+++ KilliCood.py
@@ -2,14 +2,24 @@ import os
 os.system ("clear")
 import random
 import getpass
+import time
 import sys
+def slowtype(t):
+   for D in t + "\n":
+   	sys.stdout.write(D)
+   	sys.stdout.flush()
+   	time.sleep(10/100)
+
 
 #colors 
 GG = "\033[32;1m" # Green light
 RR = "\033[31;1m" # Red light
 
+
 print("")
-print(RR+"          Welcome The Tool killi Cood")
+print("")
+print("")
+print(RR+"        [ Welcome The Tool killi Cood ] ")
 print(GG+"""
                 uu$$$$$$$$$$$uu
              uu$$$$$$$$$$$$$$$$$uu
@@ -55,9 +65,9 @@ for i in range(15):
 
 
 print(RR+"")
-print("======================")
-print("     by_HEKSOS")
-print("======================")
-print(" YouTube : MR.HEKSOS")
-print("======================")
+print("×======================×")
+slowtype("     by_HEKSOS")
+print("×======================×")
+slowtype(" YouTube : MR.HEKSOS")
+print("×======================×")
 print("")
